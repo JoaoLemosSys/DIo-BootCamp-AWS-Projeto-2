ea75b0401305856a367f3752455822942b821159
João Lemos <joao.lemos.sys@gmail.com>
2023-04-28 08:03:53 -0300

    Envio Inicial

---
+++ apache.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Implementando Servidor Web Apache..."
+
+echo "Atualizando apt-get..."
+apt-get update
+
+echo "Instalando Servidor Apache..."
+apt-get install apache2 -y
+
+echo "Instalando Unzip..."
+apt-get install unzip
+
+echo "Baixando arquivos do site de exemplo..."
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+
+echo "Descompactando..."
+unzip  /tmp/main.zip
+echo "Copiando arquivos para pasta do servidor Apache"
+cd linux-site-dio-main 
+cp *.* /var/www/html -f
+
+echo "Finalizado."
